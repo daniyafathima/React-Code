c35534e12276361b7609f202923f2d0b5f11f440
daniyafathima <daniya28fathima@gmail.com>
2026-01-21 10:52:59 +0530

    New commit

--- bsicvite/src/main.jsx
+++ bsicvite/src/main.jsx
@@ -1,9 +1,40 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App!!</h1>
+    </div>
+  )
+}
+
+// const reactElement={
+//     type:'a',
+//     props:{
+//         href:'https://google.com',
+//         target:'_blank'
+//     },
+//     children:'Click me to visit google'
+// } this can be used in custom not here because it doesnt know the type
+
+// const anotherElement=(
+//   <a href="https://google.com" target="_blank">Visit Google</a>
+// ) this cant be used because becuase the variable cant be used directly
+
+const newReactElement=React.createElement(
+'a',
+  {href:'https://google.com',target: '_blank'},
+  'Click Meee'
+)
+  
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+  newReactElement
+    
+  
 )
